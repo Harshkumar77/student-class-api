c8ae6005dfc903cb3cfd424c70fb89a3b725961b
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-08-11 11:18:19 +0530

    Addded loose error handling

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-import 'dotenv/config'
-import express from 'express'
-import { configureApp } from './config/express.config';
-import addRoutes from './routes';
+import "dotenv/config"
+import express from "express"
+import { configureApp } from "./config/express.config"
+import addRoutes from "./routes"
 
 const app = express()
 
 configureApp(app)
 
 addRoutes(app)
+
+process.on("uncaughtException", function (err) {
+  console.error(err)
+  console.log("Node NOT Exiting...")
+})
